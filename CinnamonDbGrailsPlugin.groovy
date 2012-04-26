4d88e53881bdf4d4ffc7db7a91e2e1c3fe7f8bb1
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-04-26 12:13:54 +0200

    Updated README and Plugin data.

--- CinnamonDbGrailsPlugin.groovy
+++ CinnamonDbGrailsPlugin.groovy
@@ -18,11 +18,11 @@ class CinnamonDbGrailsPlugin {
     def author = "Ingo Wiarda"
     def authorEmail = ""
     def description = '''\
-Brief summary/description of the plugin.
+Domain and base classes for the Cinnamon CMS.
 '''
 
     // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/cinnamon-db"
+    def documentation = "http://cinnamon-cms.de"
 
     // Extra (optional) plugin metadata
 
@@ -39,7 +39,7 @@ Brief summary/description of the plugin.
 //    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
 
     // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
+    def scm = [ url: "https://github.com/dewarim/cinnamon-db" ]
 
     def doWithWebDescriptor = { xml ->
         // TODO Implement additions to web.xml (optional), this event occurs before
