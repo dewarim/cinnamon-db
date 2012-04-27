fdddaf581aa605705d7456ae8082f096d63301c5
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-04-27 14:28:39 +0200

    fixed hashCode and equals

--- grails-app/domain/cinnamon/CmnGroup.groovy
+++ grails-app/domain/cinnamon/CmnGroup.groovy
@@ -76,15 +76,12 @@ class CmnGroup implements Serializable  {
         if (this.is(o)) return true
         if (!(o instanceof CmnGroup)) return false
 
-        CmnGroup userGroup = (CmnGroup) o
+        CmnGroup cmnGroup = (CmnGroup) o
 
-        if (aclEntries != userGroup.aclEntries) return false
-        if (description != userGroup.description) return false
-        if (groupOfOne != userGroup.groupOfOne) return false
-        if (groupUsers != userGroup.groupUsers) return false
-        if (id != userGroup.id) return false
-        if (name != userGroup.name) return false
-        if (parent != userGroup.parent) return false
+        if (description != cmnGroup.description) return false
+        if (groupOfOne != cmnGroup.groupOfOne) return false
+        if (name != cmnGroup.name) return false
+        if (parent != cmnGroup.parent) return false
 
         return true
     }
@@ -95,11 +92,6 @@ class CmnGroup implements Serializable  {
         result = 31 * result + (description != null ? description.hashCode() : 0)
         result = 31 * result + (groupOfOne != null ? groupOfOne.hashCode() : 0)
         result = 31 * result + (parent != null ? parent.hashCode() : 0)
-        result = 31 * result + (id != null ? id.hashCode() : 0)
-        result = 31 * result + (groupUsers != null ? groupUsers.hashCode() : 0)
-        result = 31 * result + (aclEntries != null ? aclEntries.hashCode() : 0)
         return result
     }
-
-
 }
