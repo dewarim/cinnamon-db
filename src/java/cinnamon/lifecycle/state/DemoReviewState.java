c51ba2ddbf4f0295abeaf0685e13b06870eafcf6
Ingo Wiarda <ingo_wiarda@dewarim.de>
2012-05-25 15:52:07 +0200

    continue migration to Cinnamon3: add LifeCycleState classes.

---
+++ src/java/cinnamon/lifecycle/state/DemoReviewState.java
@@ -0,0 +1,46 @@
+package cinnamon.lifecycle.state;
+
+import cinnamon.ObjectSystemData;
+import cinnamon.lifecycle.BaseLifeCycleState;
+import cinnamon.lifecycle.IState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ *
+ */
+public class DemoReviewState extends BaseLifeCycleState implements IState {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public List<IState> getExitStates(ObjectSystemData osd) {
+        return states;
+    }
+
+    @Override
+    public Boolean checkEnteringObject(ObjectSystemData osd, String params) {
+        // object must be in "authoring" state.
+        if(osd.getState() == null){
+            log.debug("osd has no lifecycle state.");
+            return false;
+        }
+        else if(osd.getState().getName().equals("authoring")){
+            return true;
+        }
+        log.debug("osd has wrong lifecycle state: "+osd.getState().getName());
+        return false;
+    }
+
+    @Override
+    public void enter(ObjectSystemData osd, String params) {
+        log.debug("osd "+osd.getId()+" entered DemoReviewState.");
+    }
+
+    @Override
+    public void exit(ObjectSystemData osd, IState nextState, String params) {
+        log.debug("osd "+osd.getId()+" left DemoReviewState.");
+    }
+}
---
+++ src/java/cinnamon/lifecycle/state/NopState.java
@@ -0,0 +1,38 @@
+package cinnamon.lifecycle.state;
+
+import cinnamon.ObjectSystemData;
+import cinnamon.lifecycle.BaseLifeCycleState;
+import cinnamon.lifecycle.IState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/**
+ * Does nothing, accepts exiting to and entering from every other state.
+ * Useful for lifecycles whose states are mostly declarative instead of functional.
+ */
+public class NopState extends BaseLifeCycleState implements IState {
+
+    Logger log = LoggerFactory.getLogger(this.getClass());
+
+    @Override
+    public List<IState> getExitStates(ObjectSystemData osd) {
+        return states;
+    }
+
+    @Override
+    public Boolean checkEnteringObject(ObjectSystemData osd, String params) {
+        return true;
+    }
+
+    @Override
+    public void enter(ObjectSystemData osd, String params) {
+        log.debug("OSD "+osd.getId()+" entered NopState");
+    }
+
+    @Override
+    public void exit(ObjectSystemData osd, IState nextState, String params) {
+        log.debug("OSD "+osd.getId()+" left NopState.");
+    }
+}
