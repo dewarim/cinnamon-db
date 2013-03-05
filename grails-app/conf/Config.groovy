15f04e0a7354fed449af06775e683f4f1a137aac
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-03-05 14:12:01 +0100

    Add logging options (for debugging).

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -1,13 +1,20 @@
 // configuration for plugin testing - will not be included in the plugin zip
 
+grails.config.locations = ["classpath:${appName}-config.properties",
+        "classpath:${appName}-config.groovy",
+        "file:${userHome}/.grails/${appName}-config.properties",
+        "file:${userHome}/.grails/${appName}-config.groovy",
+        "file:${System.env.CINNAMON_HOME_DIR}/${appName}-config.groovy"
+]
+
 log4j = {
     // Example of changing the log pattern for the default console
     // appender:
     //
-    //appenders {
-    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
-    //}
-
+    appenders {
+        console name:'stdout', layout:pattern(conversionPattern: '%c %m%n')
+    }
+    
     error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
            'org.codehaus.groovy.grails.web.pages', //  GSP
            'org.codehaus.groovy.grails.web.sitemesh', //  layouts
@@ -18,9 +25,15 @@ log4j = {
            'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
            'org.springframework',
            'org.hibernate',
-           'net.sf.ehcache.hibernate'
+           'net.sf.ehcache.hibernate',
+            'org.apache',
+            'net.sf.ehcache',
+            'org.grails.plugin.resource'
 
     warn   'org.mortbay.log'
+    root {
+        debug 'stdout'
+    }
 }
 
 
