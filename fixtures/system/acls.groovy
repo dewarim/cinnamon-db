c9820cc2659cf6c3a466472c77eaf08c65e07b10
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-03-05 17:00:50 +0100

    Fixtures for acls and folderTypes.

---
+++ fixtures/system/acls.groovy
@@ -0,0 +1,8 @@
+import cinnamon.Acl
+import cinnamon.global.Constants
+
+fixture{
+    
+    defaultAcl(Acl, name:Constants.ACL_DEFAULT)
+    
+}
---
+++ fixtures/system/folderTypes.groovy
@@ -0,0 +1,8 @@
+import cinnamon.FolderType
+import cinnamon.global.Constants
+
+fixture{
+    
+    defaultFolderType(FolderType, name:Constants.FOLDER_TYPE_DEFAULT)
+    
+}
