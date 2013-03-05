1d8b457e1c0fb8e2b33064b5fea59f3b7eb2d24b
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-03-05 16:21:42 +0100

    Initial version of objectTypes fixture.

---
+++ fixtures/system/objectTypes.groovy
@@ -0,0 +1,21 @@
+import cinnamon.ObjectType
+import cinnamon.global.Constants
+
+fixture {
+
+    renderType(ObjectType, name: Constants.OBJECT_TYPE_RENDER_TASK, config: '<meta/>')
+    cartType(ObjectType, name: Constants.OBJTYPE_CART, config: '<meta/>')
+    configType(ObjectType, name: Constants.OBJTYPE_CONFIG, config: '<meta/>')
+    renditionType(ObjectType, name: Constants.OBJTYPE_RENDITION, config: '<meta/>')
+    searchType(ObjectType, name: Constants.OBJTYPE_SEARCH, config: '<meta/>')
+    supportType(ObjectType, name: Constants.OBJTYPE_SUPPORTING_DOCUMENT, config: '<meta/>')
+    documentType(ObjectType, name: Constants.OBJTYPE_DOCUMENT, config: '<meta/>')
+    translationType(ObjectType, name: Constants.OBJTYPE_TRANSLATION_TASK, config: '<meta/>')
+    imageType(ObjectType, name: Constants.OBJTYPE_IMAGE, config: '<meta/>')
+    workflowType(ObjectType, name: Constants.OBJTYPE_WORKFLOW, config: '<meta/>')
+    workflowTemplateType(ObjectType, name: Constants.OBJTYPE_WORKFLOW_TEMPLATE, config: '<meta/>')
+    taskType(ObjectType, name: Constants.OBJTYPE_TASK, config: '<meta/>')
+    taskDefinitionType(ObjectType, name: Constants.OBJTYPE_TASK_DEFINITION, config: '<meta/>')
+    defaultType(ObjectType, name:Constants.OBJTYPE_DEFAULT, config:'<meta/>')
+    
+}
