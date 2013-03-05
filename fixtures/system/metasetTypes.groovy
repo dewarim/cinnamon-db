b63ae3f002f68f287a0ea218e3e6bdd51c67ed14
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-03-05 16:21:55 +0100

    Initial version of metasetTypes fixture.

---
+++ fixtures/system/metasetTypes.groovy
@@ -0,0 +1,13 @@
+import cinnamon.MetasetType
+
+fixture{
+    
+    searchMs(MetasetType, name:'search', config: '<metaset />')
+    cartMs(MetasetType, name:'cart', config: '<metaset />')
+    translationMs(MetasetType, name:'translation_extension',config: '<metaset />')
+    renderInputMs(MetasetType, name:'render_input', config: '<metaset />')
+    renderOutputMs(MetasetType, name:'render_output', config:'<metaset />')
+    testMs(MetasetType, name:'test', config: '<metaset />')
+    tikaMs(MetasetType, name:'tika', config: '<metaset />')
+    
+}
