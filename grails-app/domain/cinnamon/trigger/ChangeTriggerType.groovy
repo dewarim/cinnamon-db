0ccb9ea9b47719cd37062f6b29975d29fa3b7b74
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-03-06 15:29:43 +0100

    Removed obsolete description field. Removed unused constructor with String-based map-params. Removed hasMany association (bidirectional association is not required here).

--- grails-app/domain/cinnamon/trigger/ChangeTriggerType.groovy
+++ grails-app/domain/cinnamon/trigger/ChangeTriggerType.groovy
@@ -6,45 +6,42 @@ import org.dom4j.DocumentHelper
 import cinnamon.i18n.LocalMessage
 import cinnamon.global.Constants
 
-class ChangeTriggerType  implements Serializable {
+class ChangeTriggerType implements Serializable {
 
     static constraints = {
-        name unique:true, size: 1..Constants.NAME_LENGTH
-        description size: 1..Constants.DESCRIPTION_SIZE
+        name unique: true, size: 1..Constants.NAME_LENGTH
     }
 
-    static hasMany = [changeTriggers:ChangeTrigger]
-    
     static mapping = {
         cache true
         version 'obj_version'
         table 'change_trigger_types'
     }
-    
-    String name
-    String description
-    Class<? extends ITrigger> triggerClass;
 
-    public ChangeTriggerType(){
+    String name
+    Class<? extends ITrigger> triggerClass
+
+    public ChangeTriggerType() {
 
     }
 
     @SuppressWarnings("unchecked")
-    public ChangeTriggerType(Map<String,String> cmd){
-        name		= cmd.get("name");
-        description = cmd.get("description");
-        try{
+    public ChangeTriggerType(Map<String, String> cmd) {
+        name = cmd.get("name");
+        try {
             triggerClass = (Class<? extends ITrigger>) Class.forName(cmd.get("trigger_class"));
+        } catch (InstantiationException e) {
+            throw new CinnamonException("error.instantiating.class", className);
+        } catch (IllegalAccessException e) {
+            throw new CinnamonException("error.accessing.class", className);
         }
         catch (ClassNotFoundException e) {
-            throw new CinnamonException("error.loading.class",e);
+            throw new CinnamonException("error.loading.class", e);
         }
-
     }
 
-    public ChangeTriggerType(String name, String description, Class<? extends ITrigger> triggerClass){
+    public ChangeTriggerType(String name, Class<? extends ITrigger> triggerClass) {
         this.name = name;
-        this.description = description;
         this.triggerClass = triggerClass;
     }
 
@@ -54,20 +51,19 @@ class ChangeTriggerType  implements Serializable {
      * @param elementName the root element of the ChangeTriggerType's XML serialization.
      * For example, if you want
      * <pre>
-     *  {@code
+     * {@code
      *  <ctt><id>5</id><name>...</ctt>
-     * }
+     *}
      * </pre>
      * you have to pass "ctt" as elementName.
      * @param type the ChangeTriggerType that you want to serialize.
      * @return the new element.
      */
-    public static Element asElement(String elementName, ChangeTriggerType type){
+    public static Element asElement(String elementName, ChangeTriggerType type) {
         Element e = DocumentHelper.createElement(elementName);
-        if(type != null){
+        if (type != null) {
             e.addElement("id").addText(String.valueOf(type.getId()));
-            e.addElement("name").addText( LocalMessage.loc(type.getName()));
-            e.addElement("description").addText( LocalMessage.loc(type.getDescription()));
+            e.addElement("name").addText(LocalMessage.loc(type.getName()));
             e.addElement("triggerClass").addText(type.triggerClass.getName());
         }
         return e;
@@ -79,7 +75,6 @@ class ChangeTriggerType  implements Serializable {
 
         ChangeTriggerType that = (ChangeTriggerType) o
 
-        if (description != that.description) return false
         if (name != that.name) return false
         if (triggerClass != that.triggerClass) return false
 
@@ -89,7 +84,6 @@ class ChangeTriggerType  implements Serializable {
     int hashCode() {
         int result
         result = (name != null ? name.hashCode() : 0)
-        result = 31 * result + (description != null ? description.hashCode() : 0)
         result = 31 * result + (triggerClass != null ? triggerClass.hashCode() : 0)
         return result
     }
