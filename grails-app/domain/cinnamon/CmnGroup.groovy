367b219dd1c112a53f1225379ca541c99f364a83
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-03-12 12:28:55 +0100

    Add toXmlElement(root).

--- grails-app/domain/cinnamon/CmnGroup.groovy
+++ grails-app/domain/cinnamon/CmnGroup.groovy
@@ -1,6 +1,8 @@
 package cinnamon
 
 import cinnamon.global.Constants
+import cinnamon.i18n.LocalMessage
+import org.dom4j.Element
 
 class CmnGroup implements Serializable  {
 
@@ -80,5 +82,25 @@ class CmnGroup implements Serializable  {
         int result
         result = (name != null ? name.hashCode() : 0)
         return result
+    }
+
+    public void toXmlElement(Element root) {
+        Element group = root.addElement( "group" );
+        group.addElement("id").addText(String.valueOf(getId()) );
+        group.addElement( "name").addText(  LocalMessage.loc(getName()) );
+        group.addElement( "sysName").addText(getName());
+        group.addElement( "is_user").addText( groupOfOne.toString() );
+        group.addElement( "parent").addText(getParent() == null ? "null" : String.valueOf(getParent().getId())  );
+        Element subGroups= group.addElement("subGroups");
+        for(CmnGroup g: CmnGroup.findAllByParent(this)){
+            Element subGroup = subGroups.addElement("subGroup");
+            subGroup.addElement("id").addText(String.valueOf(g.getId()));
+            subGroup.addElement("name").addText(LocalMessage.loc(g.getName()));
+            subGroup.addElement("sysName").addText(g.getName());
+        }
+        Element users = group.addElement("users");
+        UserAccount.executeQuery("select ua from UserAccount ua where ua in (select cgu.userAccount from CmnGroupUser cgu where cgu.cmnGroup=:group)", [group:this]).each{ user ->
+            users.add(UserAccount.asElement("user", user));
+        }
     }
 }
