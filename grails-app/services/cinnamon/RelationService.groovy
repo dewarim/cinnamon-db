c22ca50bc0cecb22066683393d5fd717d99c49bf
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-03-19 14:15:42 +0100

    Added findOrCreateRelation.

--- grails-app/services/cinnamon/RelationService.groovy
+++ grails-app/services/cinnamon/RelationService.groovy
@@ -1,6 +1,7 @@
 package cinnamon
 
 import cinnamon.relation.Relation
+import cinnamon.relation.RelationType
 import cinnamon.relation.resolver.RelationSide
 
 class RelationService {
@@ -16,15 +17,41 @@ class RelationService {
      * use the LatestHeadResolver need to update their link to this new version.
      * @param changedOsd OSD which has already been updated (or does not need an update).
      */
-    void updateRelations(ObjectSystemData changedOsd){
-        for(ObjectSystemData osd : ObjectSystemData.findAllByRoot(changedOsd.root)){
-            for(Relation relation : Relation.findAllByLeftOSD(osd)){
+    void updateRelations(ObjectSystemData changedOsd) {
+        for (ObjectSystemData osd : ObjectSystemData.findAllByRoot(changedOsd.root)) {
+            for (Relation relation : Relation.findAllByLeftOSD(osd)) {
                 relation.leftOSD = relation.getType().findOsdVersion(relation, osd, RelationSide.LEFT);
             }
-            for(Relation relation : Relation.findAllByRightOSD(osd)){
+            for (Relation relation : Relation.findAllByRightOSD(osd)) {
                 relation.rightOSD = relation.getType().findOsdVersion(relation, osd, RelationSide.RIGHT);
             }
         }
+    }
+
+    /**
+     * Find or create a relation of the specified type and left/right objects.
+     * Does not use metadata parameter to determine if the relation already exists,
+     * but update the relation with the given metadata.
+     * @param type the type of the relation
+     * @param leftOsd the left part of the relation
+     * @param rightOsd the right part of the relation
+     * @param metadata the metadata - if the relation exists, the current metadata
+     * will be replaced.
+     * @return the new or updated relation
+     */
+    Relation findOrCreateRelation(RelationType type, ObjectSystemData leftOsd,
+                                  ObjectSystemData rightOsd, String metadata) {
+        def relation = Relation.findByTypeAndLeftOSDAndRightOSD(type, leftOsd, rightOsd)
+        if (!relation) {
+            relation = new Relation(type, leftOsd, rightOsd, metadata)
+            relation.save()
+        }
+        else{
+            if (relation.metadata != metadata){
+                relation.metadata = metadata
+            }
+        }
+        return relation
     }
 
 }
