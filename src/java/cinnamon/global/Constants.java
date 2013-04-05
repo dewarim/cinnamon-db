f99bd3ba194526212c3351b3c38751205658ce30
Ingo Wiarda <ingo_wiarda@dewarim.de>
2013-04-05 13:29:19 +0200

    Add error logging to workflows.
    When a transition fails (example: workflow server cannot upload a new document to an FTP server), the corresponding task object is set to failed and a log message is added to the 'log' metaset.
    This way, a failed workflow may be debugged without having direct access to the server's log files.

--- src/java/cinnamon/global/Constants.java
+++ src/java/cinnamon/global/Constants.java
@@ -79,10 +79,24 @@ public class Constants {
 	public static final String PROCSTATE_TASK_TODO = "todo";
 	public static final String PROCSTATE_TASK_DONE = "done";
 	public static final String PROCSTATE_TRANSITION_READY = "transition_ready";
+	public static final String PROCSTATE_TRANSITION_FAILED = "transition_failed";
 	public static final String PROCSTATE_WORKFLOW_STARTED = "running";
 	public static final String PROCSTATE_WORKFLOW_FINISHED = "finished";
 	public static final String PROCSTATE_REVIEW_OK = "review_ok";
 
+    public static final String METASET_TASK_DEFINITION = "task_definition";
+    public static final String METASET_WORKFLOW_TEMPLATE = "workflow_template";
+    public static final String METASET_LOG = "log";
+    public static final String METASET_TRANSITION = "transition";
+    public static final String METASET_RENDER_INPUT = "render_input";
+    public static final String METASET_RENDER_OUTPUT = "render_output";
+    public static final String METASET_TIKA = "tika";
+    public static final String METASET_TEST = "test";
+    public static final String METASET_TRANSLATION_EXTENSION = "translation_extension";
+    public static final String METASET_SEARCH = "search";
+    public static final String METASET_CART = "cart";
+    public static final String METASET_NOTIFICATION = "notification";
+    
     // Constants for the RenderServer extension
     public static final String RENDER_SERVER_LIFECYCLE = "_RenderServerLC";
     public static final String RENDERSERVER_RENDER_TASK_NEW = "newRenderTask";
