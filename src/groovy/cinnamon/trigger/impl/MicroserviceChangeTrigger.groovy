b3d3d70dcbbcfdf198248e3e42f48c8b8269c076
ingo.wiarda <ingo_wiarda@dewarim.de>
2015-11-22 21:23:05 +0100

    Fix MicroserviceChangeTrigger to send servlet response to remote server.

--- src/groovy/cinnamon/trigger/impl/MicroserviceChangeTrigger.groovy
+++ src/groovy/cinnamon/trigger/impl/MicroserviceChangeTrigger.groovy
@@ -1,17 +1,15 @@
 package cinnamon.trigger.impl
 
 import cinnamon.PoBox
+import cinnamon.global.ConfThreadLocal
 import cinnamon.trigger.ChangeTrigger
 import cinnamon.trigger.ITrigger
 import cinnamon.utils.ParamParser
 import org.apache.http.Header
 import org.apache.http.HttpResponse
 import org.apache.http.client.HttpClient
-import org.apache.http.client.methods.HttpPost
 import org.apache.http.client.methods.RequestBuilder
-import org.apache.http.impl.client.DefaultHttpClient
 import org.apache.http.impl.client.HttpClientBuilder
-import org.apache.http.params.BasicHttpParams
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
@@ -42,6 +40,9 @@ public class MicroserviceChangeTrigger implements ITrigger {
             def headerNames = request.headerNames
             while(headerNames.hasMoreElements()){
                 def headerName = headerNames.nextElement()
+                if(headerName.equals("microservice")){
+                    continue;
+                }
                 requestCopy.setHeader(headerName, request.getHeader(headerName))   
             }
             for(Map.Entry entry : poBox.params){
@@ -74,39 +75,9 @@ public class MicroserviceChangeTrigger implements ITrigger {
                         changeTrigger.config)
                 return poBox;
             }
+            poBox.response.addHeader("microservice", url)
+            // request will be executed later on by the ResponseFilter.
 
-            def request = poBox.request
-            HttpClient httpClient = HttpClientBuilder.create().build()
-            def requestCopy = RequestBuilder.create("POST")
-            requestCopy.setUri(url)
-            def headerNames = request.headerNames
-            while(headerNames.hasMoreElements()){
-                def headerName = headerNames.nextElement()
-                requestCopy.setHeader(headerName, request.getHeader(headerName))
-            }
-            for(Map.Entry entry : poBox.params){
-                if(entry.key instanceof String && entry.value instanceof String) {
-                    requestCopy.addParameter(entry.key, entry.value)
-                }
-                else{
-                    log.debug("Entry "+entry+" is not <String,String>")
-                }
-            }
-            for(Map.Entry entry : poBox.model){
-                requestCopy.addParameter(String.valueOf(entry.key), String.valueOf(entry.value))
-            }
-            HttpResponse httpResponse = httpClient.execute(requestCopy.build())
-            log.debug("Microservice post returned :"+ httpResponse.statusLine)
-            
-            // Note: looks like post-filters just cannot set new headers once render() has been called.
-            // this is rather unfortunate, since XML responses are always generated via render.
-            // so this is more of a proof of conecpt:
-            ByteArrayOutputStream baos = new ByteArrayOutputStream()
-            httpResponse.entity.writeTo(baos)
-            poBox.response.addHeader("ctResponse-"+ changeTrigger.triggerType.name, baos.toString("UTF-8") )
-            httpResponse.allHeaders.each { Header header ->
-                poBox.response.addHeader("ctHeader-"+changeTrigger.triggerType.name+"-"+header.name, header.value)
-            }
         }
         catch (Exception e) {
             log.debug("Failed to execute microserviceChangeTrigger.", e);
