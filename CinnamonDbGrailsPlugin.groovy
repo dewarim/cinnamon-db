9665b6c77d9faac2ecb38cfc8ede86f458c0d7e1
ingo.wiarda <ingo_wiarda@dewarim.de>
2018-02-25 14:39:24 +0100

    cinnamon-db plugin version++

--- CinnamonDbGrailsPlugin.groovy
+++ CinnamonDbGrailsPlugin.groovy
@@ -2,7 +2,7 @@ import cinnamon.Session
 
 class CinnamonDbGrailsPlugin {
 
-    def version = "3.7.25"
+    def version = "3.7.31"
     def grailsVersion = "2.4 > *"
     def dependsOn = [:]
     def pluginExcludes = [
