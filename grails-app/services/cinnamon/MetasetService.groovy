3d8c5301d61a54e73b71d21761d9eb9d41e47ccd
ingo.wiarda <ingo_wiarda@dewarim.de>
2018-12-30 18:02:26 +0100

    When copying OSDs and Folders, create fresh copies of custom metadata objects instead of linking to the existing versions. The user normally expects the copy of a document to use its own metadata and not share it with the original document.
    Fix #190, fix #191

--- grails-app/services/cinnamon/MetasetService.groovy
+++ grails-app/services/cinnamon/MetasetService.groovy
@@ -20,16 +20,16 @@ public class MetasetService {
      *
      * @param content the new content to set
      * @param metaset the Metaset to update
-     * @param owner   if branching is necessary, this object will be the owner of the new branch.
+     * @param owner if branching is necessary, this object will be the owner of the new branch.
      */
     public void updateWithBranch(String content, IMetasetOwner owner, Metaset metaset) {
         Collection<IMetasetOwner> owners = fetchOwners(owner, metaset.type);
         if (owners.size() > 1) {
             // remove existing metaset:
             IMetasetJoin metasetJoin = owner.fetchMetasetJoin(metaset.type);
-            if(metasetJoin != null){
-                if(metasetJoin.isDirty()){
-                    metasetJoin.save(flush:true)
+            if (metasetJoin != null) {
+                if (metasetJoin.isDirty()) {
+                    metasetJoin.save(flush: true)
                 }
                 metasetJoin.doDelete();
             }
@@ -50,8 +50,8 @@ public class MetasetService {
     public Collection<IMetasetOwner> fetchOwners(Metaset metaset) {
 
         // fetch OsdMetasets and FolderMetasets:
-        List<IMetasetOwner> osdMetasets = ObjectSystemData.findAll("from OsdMetaset o where o.metaset=:metaset", [metaset:metaset])
-        List<IMetasetOwner> folderMetasets = Folder.findAll("from FolderMetaset fm where fm.metaset=:metaset", [metaset:metaset]);
+        List<IMetasetOwner> osdMetasets = ObjectSystemData.findAll("from OsdMetaset o where o.metaset=:metaset", [metaset: metaset])
+        List<IMetasetOwner> folderMetasets = Folder.findAll("from FolderMetaset fm where fm.metaset=:metaset", [metaset: metaset]);
 
         // combine collections:
         List<IMetasetOwner> owners = new ArrayList<IMetasetOwner>(osdMetasets.size() + folderMetasets.size());
@@ -61,13 +61,13 @@ public class MetasetService {
     }
 
     public void unlinkMetaset(IMetasetOwner owner, Metaset metaset) {
-        if(! metaset){
+        if (!metaset) {
             // nop. Example use case:
             // unlinkMetaset(osd, osd.fetchMetaset(thumbnail))
             return
         }
         IMetasetJoin metasetJoin = owner.fetchMetasetJoin(metaset.type);
-        if(! metasetJoin){
+        if (!metasetJoin) {
             log.warn("metaset join for metaset ${metaset.id} was not found.")
             return
         }
@@ -75,18 +75,18 @@ public class MetasetService {
         def metaCount = FolderMetaset.countByMetaset(metaset) + OsdMetaset.countByMetaset(metaset)
         if (metaCount == 0) {
             // only delete a metaset if no one else links there:
-            if(metaset.folderMetasets.size() > 0){
+            if (metaset.folderMetasets.size() > 0) {
                 log.warn("Metaset #${metaset.id} still has folderMetasets. Something's wrong.")
-                metaset.folderMetasets.clear()                
+                metaset.folderMetasets.clear()
             }
-            if(metaset.osdMetasets.size() > 0){
+            if (metaset.osdMetasets.size() > 0) {
                 log.warn("Metaset #${metaset.id} still has osdMetasets. Something's wrong.")
             }
             metaset.delete()
         }
     }
 
-    public Metaset createMetaset(owner, metasetType, content){
+    public Metaset createMetaset(owner, metasetType, content) {
         log.debug("create new metaset")
         def metaset = new Metaset(content, metasetType);
         owner.save() // we need the Hibernate Id to add a Metaset
@@ -94,7 +94,7 @@ public class MetasetService {
         owner.addMetaset(metaset);
         return metaset
     }
-    
+
     public Metaset createOrUpdateMetaset(IMetasetOwner owner, MetasetType metasetType, String content, WritePolicy writePolicy) {
         Metaset metaset = owner.fetchMetaset(metasetType.getName());
         if (metaset == null) {
@@ -116,24 +116,26 @@ public class MetasetService {
         return metaset;
     }
 
-    // TODO: remove?
-    public void copyMetasets(IMetasetOwner source, IMetasetOwner target, String metasets) {
-        if (metasets == null) {
-            for (Metaset m : source.fetchMetasets()) {
-                log.debug(String.format("Add Metaset %d %s from %d to %d", m.getId(), m.getType().getName(), source.getId(), target.getId()));
-                target.addMetaset(m);
-            }
+    public void copyMetasets(IMetasetOwner source, IMetasetOwner target, String metasetNames) {
+        List<Metaset> metasets = new ArrayList<>()
+        if (metasetNames == null) {
+            metasets.addAll(source.fetchMetasets())
         } else {
-            for (String metasetName : metasets.split(",")) {
-                Metaset metaset = source.fetchMetaset(metasetName);
-                target.addMetaset(metaset);
-            }
+            def names = Arrays.asList(metasetNames.split(","))
+            names.forEach { name -> metasets.add(source.fetchMetaset(name)) }
+        }
+
+        for (Metaset m : metasets) {
+            log.debug(String.format("Add Metaset %d %s from %d to %d", m.getId(), m.getType().getName(), source.getId(), target.getId()));
+            Metaset metasetCopy = new Metaset(m.content, m.type)
+            metasetCopy.save()
+            target.addMetaset(metasetCopy);
         }
     }
 
     // TODO: remove? may be leftover from metaset field to class conversion
     public void initializeMetasets(IMetasetOwner owner, String metasets) {
-        if(metasets == null){
+        if (metasets == null) {
             // nothing to do.
             return;
         }
